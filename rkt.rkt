a7b3b710ba43bdf2bfe8af1eaf92c6486a55419a
Dmitry Ponyatov <dponyatov@gmail.com>
2016-08-21 20:04:55 +0400

    * 2 parsing
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- rkt.rkt
+++ rkt.rkt
@@ -3,9 +3,7 @@
 (define-type ArithC ; base algebraic type 
   [numC  (n : number) ] ; number
   [plusC (l : ArithC) (r : ArithC) ] ; + operator
-  [minusC (l : ArithC) (r : ArithC) ] ; - operator
   [multC (l : ArithC) (r : ArithC) ] ; * operator
-  [divC (l : ArithC) (r : ArithC) ] ; / operator
   )
 
 (define (parse [s : s-expression]) : ArithC ; type-setting parser
@@ -15,12 +13,13 @@
      (let ([sl (s-exp->list s)])
        (case (s-exp->symbol (first sl))
          [(+) (plusC (parse (second sl)) (parse (third sl)))]
-         [(-) (minusC (parse (second sl)) (parse (third sl)))]
          [(*) (multC (parse (second sl)) (parse (third sl)))]
-         [(/) (divC (parse (second sl)) (parse (third sl)))]
          [else (error 'parse "invalid list input")]))]
     [else (error 'parse "invalid input")])
   )
 
-(parse '(+ 23 (- 5 6)) )
+(test
+ (parse '(+ 2 (* 2 3)) )
+ (plusC (numC 2) (multC (numC 2) (numC 3)))
+ )
 
