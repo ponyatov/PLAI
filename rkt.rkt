55b932f9f99625f6ce7b62f184c0bd000dc016a1
Dmitry Ponyatov <dponyatov@gmail.com>
2016-08-26 17:33:49 +0400

    + 5. functions
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- rkt.rkt
+++ rkt.rkt
@@ -74,3 +74,8 @@
  (multC (numC 2) (plusC (numC 3) (multC (numC -1) (numC 4))))
  )
 
+;;; 5. Functions
+
+'(define (double x) (+ x x))
+'(define (quadruple x) (double (double x)))
+'(define (const5 _) 5)
