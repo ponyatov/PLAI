66d62509518b0c0049dcaeb6e00e489cab7bd0d3
Dmitry Ponyatov <dponyatov@gmail.com>
2016-10-26 16:36:25 +0400

    + cpp/ minimal parser (REPL)
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp/cpp.cpp
+++ cpp/cpp.cpp
@@ -1,2 +1,8 @@
 #include "hpp.hpp"
+#define YYERR "\n\n["<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
+int yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
+int main() { return yyparse(); }
+
+Sym::Sym(string V) { val=V; }
+string Sym::dump() { return "<"+val+">"; }
 
--- cpp/hpp.hpp
+++ cpp/hpp.hpp
@@ -2,6 +2,21 @@
 #define _H_HPP
 
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+
+struct Sym {
+	string val;
+	Sym(string);
+	virtual string dump();
+};
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+#define TOC(X) { yylval.o = new Sym(yytext); return X; }
+extern int yyparse();
+extern int yyerror(string);
+#include "ypp.tab.hpp"
 
 #endif // _H_HPP
