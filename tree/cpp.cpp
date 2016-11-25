f6a15c4b8ca8c60c96bbb26b02a1fa877156028c
Dmitry Ponyatov <dponyatov@gmail.com>
2016-11-25 09:47:03 +0400

    + tree/ tree parser
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- tree/cpp.cpp
+++ tree/cpp.cpp
@@ -2,3 +2,16 @@
 #define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
 int main() { return yyparse(); }
+
+Sym::Sym(string V) { val=V; }
+void Sym::push(Sym*o) { nest.push_back(o); }
+
+string Sym::head() { ostringstream os; os << val << " # " << this;
+	return os.str(); }
+string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
+string Sym::dump(int depth) { string S = "\n"+pad(depth)+head();
+	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
+		S += (*it)->dump(depth+1);
+	return S; }
+
+Vector::Vector():Sym("[]"){}
--- tree/hpp.hpp
+++ tree/hpp.hpp
@@ -2,13 +2,20 @@
 #define _H_HPP
 
 #include <iostream>
+#include <sstream>
 #include <cstdlib>
 #include <vector>
 #include <map>
 using namespace std;
 
 struct Sym {
+	string val;
+	Sym(string);
+	vector<Sym*> nest; void push(Sym*);
+	virtual string head(); string pad(int); virtual string dump(int=0);
 };
+
+struct Vector:Sym { Vector(); };
 
 extern int yylex();
 extern int yylineno;
