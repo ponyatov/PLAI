15379c05d18262acb206e3814bef3a80d3ad2d57
Dmitry Ponyatov <dponyatov@gmail.com>
2016-12-05 16:24:36 +0400

    + o/
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ o/cpp.cpp
@@ -0,0 +1,17 @@
+#include "hpp.hpp"
+#define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
+void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
+int main() { return yyparse(); }
+
+Sym::Sym(string T, string V) { tag=T; val=V; }
+Sym::Sym(string V):Sym("sym",V){}
+void Sym::push(Sym*o) { nest.push_back(o); }
+
+string Sym::head() { return "<"+tag+":"+val+">"; }
+string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
+string Sym::dump(int depth) { string S = "\n"+pad(depth)+head();
+	for (auto it=lookup.begin(),e=lookup.end();it!=e;it++)
+		S += "\n"+pad(depth+1)+it->first+" = "+it->second->head();
+	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
+		S += (*it)->dump(depth+1);
+	return S; }
---
+++ o/hpp.hpp
@@ -0,0 +1,26 @@
+#ifndef _H_HPP
+#define _H_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Sym {
+	string tag; string val;
+	Sym(string,string); Sym(string);
+	vector<Sym*> nest; void push(Sym*);
+	map<string,Sym*> lookup;
+	virtual string head(); string pad(int); virtual string dump(int=0);
+};
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+extern int yyparse();
+extern void yyerror(string);
+#include "ypp.tab.hpp"
+
+#endif // _H_HPP
